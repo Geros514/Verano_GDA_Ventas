e601349d172fa863a14a30bb08e38290991760fe
Geros514 <20030315@itesa.edu.mx>
2024-06-19 10:04:33 -0600

    Versión08. Seed Db Ver1 Terminado.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -6,67 +6,96 @@ using Sales.Shared.Entities;
 namespace Sales.API.Controllers
 {
 
-    [ApiController]
-    [Route("api/countries")]
+	[ApiController]
+	[Route("api/countries")]
 
-    public class CountriesController : ControllerBase
-    {
-        private readonly DataContext _context;
+	public class CountriesController : ControllerBase
+	{
+		private readonly DataContext _context;
 
-        public CountriesController(DataContext context) 
-        { 
-            this._context = context;
-        }
+		public CountriesController(DataContext context)
+		{
+			this._context = context;
+		}
 
-        [HttpPost]
-        public async Task<ActionResult> PostAsync(Country country)
-        {
-            _context.Add(country);
+		[HttpPost]
+		public async Task<ActionResult> PostAsync(Country country)
+		{
+			try
+			{
+				_context.Add(country);
 
-            await _context.SaveChangesAsync();  
-            return Ok(country);
-        }
+				await _context.SaveChangesAsync();
+				return Ok(country);
+			}
+			catch (DbUpdateException dbUpdateException)
+			{
+				if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+				{
+					return BadRequest("Ya existe un país con el mismo nombre");
+				}
+				return BadRequest("Ya existe un país con el mismo nombre");
+				//return BadRequest(dbUpdateException.Message);
+			}
+			catch (Exception exception)
+			{
+				return BadRequest(exception.Message);
+			}
+		}
 
-        //Metodo Get
-        [HttpGet]
-        public async Task<IActionResult> GetAsync()
-        {
-            return Ok(await _context.Countries.ToListAsync());
-        }
-        [HttpGet("{id:int}")]
+		//Metodo Get
+		[HttpGet]
+		public async Task<IActionResult> GetAsync()
+		{
+			return Ok(await _context.Countries.ToListAsync());
+		}
+		[HttpGet("{id:int}")]
 
-        public async Task<IActionResult> GetAsync(int id)
-        {
-            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
-            if (country == null)
-            {
-                return NotFound();
-            }
-            return Ok(country);
-        }
+		public async Task<IActionResult> GetAsync(int id)
+		{
+			var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+			if (country == null)
+			{
+				return NotFound();
+			}
+			return Ok(country);
+		}
 
-        [HttpPut]
-        public async Task<ActionResult> PutAsync(Country country)
-        {
+		[HttpPut]
+		public async Task<ActionResult> PutAsync(Country country)
+		{
+			try
+			{
+				_context.Update(country);
+				await _context.SaveChangesAsync();
+				return Ok(country);
+			}
+			catch (DbUpdateException dbUpdateException)
+			{
+				if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+				{
+					return BadRequest("Ya existe un país con el mismo nombre");
+				}
+				return BadRequest("Ya existe un país con el mismo nombre");
+			}
+			catch (Exception exception)
+			{
+				return BadRequest(exception.Message);
+			}
+		}
 
+		[HttpDelete("{id:int}")]
+		public async Task<IActionResult> DeleteAsync(int id)
+		{
+			var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+			if (country == null)
+			{
+				return NotFound();
+			}
+			_context.Remove(country);
+			await _context.SaveChangesAsync();
+			return NoContent();
 
-            _context.Update(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
-        }
-
-        [HttpDelete("{id:int}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
-            if(country == null)
-            {
-                return NotFound();
-            }
-            _context.Remove(country);
-            await _context.SaveChangesAsync();
-            return NoContent();
-                
-        }
-    }
+		}
+	}
 }
